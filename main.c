5e603ba93f3bcdf0f6514d9e2dea1b23bbfc89c4
JuanPVXW <78832708+JuanPVXW@users.noreply.github.com>
2021-08-05 00:46:37 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,87 @@
+/*
+ * File:   main.c
+ * Author: Pablo
+ * Ejemplo de uso de I2C Master
+ * Created on 17 de febrero de 2020, 10:32 AM
+ */
+//*****************************************************************************
+// Palabra de configuraci�n
+//*****************************************************************************
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (RCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+//*****************************************************************************
+// Definici�n e importaci�n de librer�as
+//*****************************************************************************
+#include <stdint.h>
+#include <pic16f887.h>
+#include "I2C.h"
+#include <xc.h>
+//*****************************************************************************
+// Definici�n de variables
+//*****************************************************************************
+#define _XTAL_FREQ 4000000
+uint8_t contador = 0b00001111;
+//*****************************************************************************
+// Definici�n de funciones para que se puedan colocar despu�s del main de lo 
+// contrario hay que colocarlos todas las funciones antes del main
+//*****************************************************************************
+void setup(void);
+
+//*****************************************************************************
+// Main
+//*****************************************************************************
+void main(void) {
+    setup();
+    while(1){
+        I2C_Master_Start();    //Inicia la transmis de datos en SDA SEN = 1
+        I2C_Master_Write(0x50); //cargamos registro 101 0000 (escribir))
+        I2C_Master_Write(contador);    //byte de informacion
+        I2C_Master_Stop();  //transmision de datos terminado
+        __delay_ms(200);
+       
+        I2C_Master_Start();
+        I2C_Master_Write(0x51);     //cargamos misma direccion pero modo lectura
+        PORTB = I2C_Master_Read(0);
+        I2C_Master_Stop();
+        __delay_ms(200);
+        //PORTB++;   
+    }
+    return;
+}
+//*****************************************************************************
+// Funci�n de Inicializaci�n
+//*****************************************************************************
+void setup(void){
+    ANSEL = 0;
+    ANSELH = 0;
+    
+    TRISB = 0;
+    TRISD = 0;
+    
+    //PORTB = 0;
+    PORTD = 0;
+    
+    I2C_Master_Init(100000);        // Inicializar Comuncaci�n I2C
+    //configuración del reloj (oscilador interno)
+    OSCCONbits. IRCF2 = 1; 
+    OSCCONbits. IRCF1 = 1;
+    OSCCONbits. IRCF0 = 0;  //4Mhz
+    OSCCONbits. SCS = 1;
+}
